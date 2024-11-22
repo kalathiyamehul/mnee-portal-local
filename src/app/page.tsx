cb4b3d055db37232e43cd76100fb0a8c2f71e6fc
Satchmo <rohenaz@gmail.com>
2024-11-22 17:43:02 -0500

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@
 import { useState, useCallback, useEffect } from "react";
 import { Addresses, Balance, SignatureRequest, SignatureResponse, useYoursWallet } from "yours-wallet-provider";
 import { useMutation } from "@tanstack/react-query";
-import { P2PKH, PublicKey, Script, Transaction, UnlockingScript } from "@bsv/sdk";
+import { P2PKH, Script, Transaction, UnlockingScript } from "@bsv/sdk";
 import { toBitcoin, toToken, toTokenSat } from "satoshi-token";
-import P2PKHApprovedTemplate from "@/templates/p2pkhApproved";
+// import P2PKHApprovedTemplate from "@/templates/p2pkhApproved";
 import { applyInscription, Inscription } from "js-1sat-ord";
 import { Utils } from "@bsv/sdk";
 import toast from "react-hot-toast";
