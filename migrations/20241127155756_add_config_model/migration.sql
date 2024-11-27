9445929d73cee40d8306ec0ef6f2db9046e293de
David Case <david.case@shruggr.cloud>
2024-11-27 17:53:19 +0000

    rename config and rebuild migration

--- migrations/20241127155756_add_config_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Config" (
-    "id" INTEGER NOT NULL DEFAULT 1,
-    "fees" JSONB NOT NULL,
-    "feeAddress" TEXT NOT NULL,
-    "tokenId" TEXT NOT NULL,
-
-    CONSTRAINT "Config_pkey" PRIMARY KEY ("id")
-);
