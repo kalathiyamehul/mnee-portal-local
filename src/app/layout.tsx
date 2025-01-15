3d0ca464261cbf7770f09ed89d5ee838e98176e9
Luke Rohenaz <rohenaz@gmail.com>
2025-01-15 02:47:55 -0500

    change back default theme to business

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="forest">
+    <html lang="en" data-theme="business">
       <body className={inter.className}>
         <Providers>
           <CustomerProvider>
