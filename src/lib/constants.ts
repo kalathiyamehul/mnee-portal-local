bd1572149f5dfe719a2476ed821e1003d60cd56b
Luke Rohenaz <rohenaz@gmail.com>
2025-01-16 13:14:14 -0500

    add MNEE Icon

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,4 +7,4 @@ export const MNEE_TOKEN_NAME = 'MNEE Token'
 export const MNEE_TOKEN_MAX = Number.MAX_SAFE_INTEGER
 export const MNEE_TOKEN_DEC = 5n
 
-export const MNEE_TOKEN_ICON = '<icon_outpoint>'
+export const MNEE_TOKEN_ICON = '0232e2292e9c48771c29a5aaf6513102e1891a9c9a12d5726da5b10d2c2fc039_0'
