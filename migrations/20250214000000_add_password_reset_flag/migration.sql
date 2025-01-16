6e64d8dce25afef87c830ac7d3eca872874baf5a
Satchmo <rohenaz@gmail.com>
2025-01-15 20:56:02 -0500

    fix: add conditional check for adding requires_password_reset column to User table

--- migrations/20250214000000_add_password_reset_flag/migration.sql
+++ migrations/20250214000000_add_password_reset_flag/migration.sql
@@ -1,2 +1,8 @@
 -- Add requires_password_reset field to User table with default true for new users
-ALTER TABLE "user" ADD COLUMN "requires_password_reset" BOOLEAN NOT NULL DEFAULT true; 
+DO $$ 
+BEGIN 
+    IF NOT EXISTS(SELECT 1 FROM information_schema.columns 
+                 WHERE table_name='user' AND column_name='requires_password_reset') THEN
+        ALTER TABLE "user" ADD COLUMN "requires_password_reset" BOOLEAN NOT NULL DEFAULT true;
+    END IF;
+END $$; 
