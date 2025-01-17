ab3ee2806167cf72ef02fb2bb9e51e34c39cb737
Satchmo <rohenaz@gmail.com>
2025-01-16 19:56:16 -0500

    dont protect user check

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,6 +41,6 @@ export const config = {
 	matcher: [
 		"/dash/:path*",
 		"/reset-password",
-		"/api/((?!auth|config|deploy|resetPassword).*)/:path*", // Protect all API routes except /api/auth/*, /api/config, /api/deploy, and /api/resetPassword
+		"/api/((?!auth|config|deploy|resetPassword|users/check).*)/:path*", // Protect all API routes except /api/auth/*, /api/config, /api/deploy, /api/resetPassword, and /api/users/check
 	],
 };
