45c9e6986b2f50aeda437dfb5076d15b473fe142
Satchmo <rohenaz@gmail.com>
2025-01-27 11:21:46 -0500

    remove unused ref

--- src/components/pages/dash/setup/EnvWarning.tsx
+++ src/components/pages/dash/setup/EnvWarning.tsx
@@ -29,7 +29,7 @@ export function EnvWarning({ onMissingVarsChange }: EnvWarningProps) {
 
       // Check backend variables using server action
       try {
-        const { hasMintWif, hasBurnWif, hasOrdinalsService } = await checkServerEnvVars();
+        const { hasMintWif, hasBurnWif } = await checkServerEnvVars();
         if (!isMounted) return;
         if (!hasMintWif) missing.push('MINT_WIF');
         if (!hasBurnWif) missing.push('BURN_WIF');
