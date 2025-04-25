3bd04072be29f6f9aa3d7b85b2517e75c468385d
kalathiya.mehul <140096665+kalathiyamehul@users.noreply.github.com>
2025-04-25 11:52:19 +0530

    Revert "Create migration.sql"
    This reverts commit 92f51b67364883d0491d44cc65cd01dd690fb804.

--- migrations/20250425044030_no_of_approval_added/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "blacklist_request" ADD COLUMN     "no_of_approvals" INTEGER NOT NULL DEFAULT 2;
-
--- AlterTable
-ALTER TABLE "freeze_request" ADD COLUMN     "no_of_approvals" INTEGER NOT NULL DEFAULT 2;
-
--- AlterTable
-ALTER TABLE "mint_request" ALTER COLUMN "no_of_approvals" SET DEFAULT 2;
