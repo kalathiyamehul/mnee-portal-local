b42341452df121791f203fbfed59d06a2bff394a
Uday <udaybharadwa2002@gmail.com>
2025-05-09 17:31:20 +0530

    Fixed Build Errors

--- src/components/pages/dash/content/admin/index.tsx
+++ src/components/pages/dash/content/admin/index.tsx
@@ -202,12 +202,6 @@ export default function AdminPage({ defaultTab = "activity" }: AdminPageProps) {
           a.address === address
       );
 
-      const reason = activities.filter(
-        (a) =>
-          (a.type === "FREEZE" || a.type === "BLACKLIST") &&
-          a.reason === activity.reason
-      );
-
       // Get latest freeze action
       const latestFreezeAction = addressActions
         .filter((a) => a.type === "FREEZE" && a.status === "APPROVED")
