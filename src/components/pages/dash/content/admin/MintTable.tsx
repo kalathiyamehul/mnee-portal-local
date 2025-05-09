2540b8b153c8e401fa1245d330394e13ba5b4921
Uday <udaybharadwa2002@gmail.com>
2025-05-09 17:34:27 +0530

    Fixed Build Errors

--- src/components/pages/dash/content/admin/MintTable.tsx
+++ src/components/pages/dash/content/admin/MintTable.tsx
@@ -89,7 +89,7 @@ const MintTableContent = ({
         router.push("/setup?fromDashAdminMints=true");
         return;
       }
-      setConfig(config as Config);
+      setConfig(config as unknown as Config);
     };
 
     getAndSetConfig();
