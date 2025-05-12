dbda0e4616cebc635b3b7a10d1dea9353d47ef23
Uday <udaybharadwa2002@gmail.com>
2025-05-12 17:56:02 +0530

    Add Number of Approvals in Whitelisting UI Flow

--- src/components/pages/dash/Sidebar.tsx
+++ src/components/pages/dash/Sidebar.tsx
@@ -23,6 +23,7 @@ const menuItems = [
   { name: "Dashboard", href: "/dash", icon: TbActivityHeartbeat },
   { name: "Wallet", href: "/dash/wallet", icon: FaWallet },
   { name: "Customers", href: "/dash/customers", icon: FaUsers },
+  { name: "Admin", href: "/dash/admin", icon: FaGear },
   { name: "Transactions", href: "/dash/transactions", icon: FaExchangeAlt },
   { name: "Activity", href: "/dash/activity", icon: FaHistory },
   { name: "Config", href: "/dash/settings", icon: FaSliders },
