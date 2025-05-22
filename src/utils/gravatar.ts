085f63818cd329aee4d8b1b50a9838f9ac67a11e
kalathiya.mehul <140096665+kalathiyamehul@users.noreply.github.com>
2025-05-22 10:17:24 +0530

    [QUILL] Gravatar MD5 to SHA256

--- src/utils/gravatar.ts
+++ src/utils/gravatar.ts
@@ -1,12 +1,13 @@
-import md5 from 'md5';
+import SHA256 from 'crypto-js/sha256';
+import Hex from 'crypto-js/enc-hex';
 
 export const getGravatarUrl = (email: string | undefined, size = 40) => {
   if (!email) return `https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&s=${size}`;
-  const hash = md5(email.toLowerCase().trim());
+  const hash = SHA256(email.toLowerCase().trim()).toString(Hex);
   return `https://www.gravatar.com/avatar/${hash}?d=mp&s=${size}`;
 }; 
 
 // const getGravatarUrl = (email: string) => {
-//   const hash = md5(email.toLowerCase().trim());
+//   const hash = SHA256(email.toLowerCase().trim()).toString(Hex);
 //   return `https://www.gravatar.com/avatar/${hash}?d=mp&s=40`;
 // };
