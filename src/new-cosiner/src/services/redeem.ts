4ebe4d0fae825ab1b08fd66d551fccce3432342d
kalathiya.mehul <140096665+kalathiyamehul@users.noreply.github.com>
2025-07-15 14:51:58 +0530

    Update Repo

--- src/new-cosiner/src/services/redeem.ts
+++ src/new-cosiner/src/services/redeem.ts
@@ -50,3 +50,53 @@ export const createRedeemTx = async (latestDeployedTokenTxHex: string, latestDep
   };
 
 }
+export const createRedeemTxAfter = async (latestDeployedTokenTxHex: string, latestDeployedTokenOpIndex: number, redeemUtxoTxHex: string, redeemUtxoIndex: number, tokenID: string, redeemUtxoPrivKey: string): Promise<{ txHex: string; err: Error | null }> => {
+
+    const latestParentDeployedTx = Transaction.fromHex(latestDeployedTokenTxHex)
+    const redeemParentTx = Transaction.fromHex(redeemUtxoTxHex)
+    const redeemPk: PrivateKey = PrivateKey.fromWif(redeemUtxoPrivKey)
+
+    let tx = new Transaction()
+    const pk = PrivateKey.fromWif(MINT_WIF);
+
+    // parse this locking script to get the current supply
+    // latestParentDeployedTx.outputs[latestParentDeployedTx.outputs.length - 1].lockingScript
+
+    let parsedLatestMinterUtxoAmount;
+    let redeemAmount;
+    let parsedCurrentSupply;
+
+    let deployTxInstanceIp: TransactionInput = {
+        sourceTransaction: latestParentDeployedTx,
+        sourceOutputIndex: latestDeployedTokenOpIndex,
+        unlockingScriptTemplate: new OrdP2PKH().unlock(pk, "all", true)
+    }
+
+    tx.addInput(deployTxInstanceIp)
+
+    let redeemTxInstanceIp: TransactionInput = {
+        sourceTransaction: redeemParentTx,
+        sourceOutputIndex: redeemUtxoIndex,
+        unlockingScriptTemplate: new CosignTemplate().userUnlock(redeemPk, "all", true)
+    }
+
+    tx.addInput(redeemTxInstanceIp)
+
+    let fileHex = createDeployTransferInscription((parsedLatestMinterUtxoAmount! + redeemAmount!), (parsedCurrentSupply! - redeemAmount!), "redeem", tokenID)
+
+    let deployTxInstanceOp: TransactionOutput = {
+        satoshis: 1,
+        lockingScript: new OrdP2PKH().lock(MINT_ADDRESS, {
+            dataHex: fileHex
+        } as Inscription)
+    }
+
+    tx.addOutput(deployTxInstanceOp)
+
+    await tx.sign()
+
+    return {
+        txHex: tx.toHex(),
+        err: null
+    };
+}
